caedc1e5308b9cbbf68c4bcbdfd331d0c77c16bc
Christopher Young <christopheryoung@MacBook123.local>
2022-06-03 16:55:43 -0700

    updated icons & added footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 // Components
 import Streamers from './components/streamers/StreamersContainer';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div id="app">
       <Streamers/>
+      <Footer/>
     </div>
   );
 }
---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+export default function Footer(props) {
+  return (
+    <div id="footer">
+      <a href="https://www.flaticon.com/free-icons/twitch" title="twitch icons" target="_blank" rel="noreferrer">Twitch icons created by Ruslan Babkin - Flaticon</a>
+    </div>
+  );
+};
