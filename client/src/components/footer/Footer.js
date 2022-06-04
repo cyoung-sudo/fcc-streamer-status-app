ad81098fc62f68ba9e52ee61e44792b2e1c4e4e7
Christopher Young <christopheryoung@MacBook123.local>
2022-06-03 17:11:47 -0700

    updated icon & footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 export default function Footer(props) {
   return (
     <div id="footer">
-      <a href="https://www.flaticon.com/free-icons/twitch" title="twitch icons" target="_blank" rel="noreferrer">Twitch icons created by Ruslan Babkin - Flaticon</a>
+      <a href="https://www.flaticon.com/free-icons/twitch" title="twitch icons" target="_blank" rel="noreferrer">Twitch icons created by riajulislam - Flaticon</a>
     </div>
   );
 };
