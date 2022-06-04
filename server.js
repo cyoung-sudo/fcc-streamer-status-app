83a91ee5fb3f945b9681b085d1e7a00f4995cb3d
Christopher Young <christopheryoung@MacBook123.local>
2022-06-03 17:37:18 -0700

    changes for heroku deployment

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
-const path = require("path");
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/twitchApi"));
 
-//--- Serve static files from the client folder
-app.use(express.static(path.join(__dirname, "client/build")));
-
-//--- Load the React "index" for any "GET" routes not defined above
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-})
+if(process.env.NODE_ENV === 'production') {
+  // Serve static files from the client folder
+  app.use(express.static('client/build'));
+  // Load the React "index" for any "GET" routes not defined above
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
  
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
